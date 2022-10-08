746d807cb7c1a1d977964f7abe89dfa0e2e991b8
Kavipriya418 <113329595+Kavipriya418@users.noreply.github.com>
2022-10-08 10:24:40 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+
+
+
+int i;
+ 
+ 
+   printf("\nC program to print Even numbers : ");
+ 
+   for ( i = 1 ; i < 20; i++ )
+   {
+      i = i+1;
+      printf("%d  ", i);
+     
+   }
+ 
+
+}
+
+
